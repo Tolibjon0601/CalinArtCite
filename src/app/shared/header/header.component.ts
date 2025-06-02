2616e83e63ef9209db23d8f621c318f3377cea08
Tolibjon0601 <egamberdiyevtolibjon0601@gmail.com>
2025-06-02 12:44:26 +0500

    header active btn

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterModule } from '@angular/router';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule,MatIconModule,CommonModule],
+  imports: [RouterModule,MatIconModule,CommonModule,RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
